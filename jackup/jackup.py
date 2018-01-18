795eb03a4b939234648816f1fc79056777d7dfec
Jens <jensecj@gmail.com>
2018-01-18 22:15:22 +0100

    moved system utility functions to sysutils.py

--- jackup/jackup.py
+++ jackup/jackup.py
@@ -6,71 +6,16 @@ import argparse
 import json
 import subprocess
 import tableprinter as tp
+import sysutils as su
 
-def mountpoint_from_uuid(uuid):
+def can_connect(host, port):
     """
-    Returns the path where a device with UUID is mounted.
-    If the device is not mounted, then return the empty string.
+    Returns whether we can connect to a host through ssh.
     """
-    cmd_findmnt = subprocess.run(["findmnt", "-rn", "-S", "UUID=" + uuid, "-o", "TARGET"], stdout=subprocess.PIPE)
-    return str(cmd_findmnt.stdout, 'utf-8', 'ignore').strip()
+    cmd_ssh = subprocess.run(['ssh', '-p', port, host, 'exit 0'])
 
-# uuids = ["28B88482B884506C", "027E2FC17E2FAC7B"]
-# for uuid in uuids:
-#     mnt_point = mountpoint_from_uuid(uuid)
-#     if mnt_point:
-#         print(uuid + " is mounted at " + mnt_point)
-#     else:
-#         print(uuid + " is not mounted")
-
-
-
-
-def uuid_relpath_pair_from_path(path):
-    """
-    Generates a (UUID, relative path) pair from a path on the file system.
-    This is done by figuring out which device the path belongs to, and then
-    finding its mountpoint, the relative path is then calculated based on where
-    the device is mounted.
-    """
-    cmd_df = subprocess.run(['df', '--output=source', path], stdout=subprocess.PIPE)
-    device = str(cmd_df.stdout, 'utf-8', 'ignore')[10:].strip()
-
-    cmd_lsblk = subprocess.run(['lsblk', '-f', device, '-oUUID'], stdout=subprocess.PIPE)
-    uuid = str(cmd_lsblk.stdout, 'utf-8', 'ignore')[4:].strip()
-
-    cmd_mountpoint = subprocess.run(['lsblk', '-f', device, '-oMOUNTPOINT'], stdout=subprocess.PIPE)
-    mountpoint = str(cmd_mountpoint.stdout, 'utf-8', 'ignore')[10:].strip()
-
-    uuid_relative_path = path
-    if path.startswith(mountpoint):
-        uuid_relative_path = path[len(mountpoint):].strip('/')
-
-    return (uuid, uuid_relative_path)
-
-# paths = ["/mnt/extern/images", "/home/jens/test"]
-# for p in paths:
-#     print(uuid_relpath_pair_from_path(p))
-
-
-
-
-def path_from_uuid_relpath(uuid, relpath):
-    """
-    Reifies a filesystem path from a (UUID, relative path) pair.
-    This is done by finding the mountpoint of the device belonging to the UUID, then
-    glueing the relative path onto mountpoint.
-    """
-    mnt_point = mountpoint_from_uuid(uuid)
-    return os.path.join(mnt_point, relpath)
-
-# uuid_paths = [["027E2FC17E2FAC7B", "images"],
-#               ["d047f2a2-6a9b-4f9c-9c3b-bd3c418babe9", "home/jens/vault"]]
-# for uu,pp in uuid_paths:
-#     print(uu + " + " + pp + " = " + path_from_uuid_relpath(uu,pp))
-
-
-
+    # 0 means no errors, c-style.
+    return not cmd_ssh.returncode
 
 def is_jackup_repo(config):
     """
@@ -99,15 +44,6 @@ def init(config):
         json.dump({ "master": config['master'], "slaves": [] }, jackup_db)
 
     print("Initialized a new repository in " + config['master'])
-
-def can_connect(host, port):
-    """
-    Returns whether we can connect to a host through ssh.
-    """
-    cmd_ssh = subprocess.run(['ssh', '-p', port, host, 'exit 0'])
-
-    # 0 means no errors, c-style.
-    return not cmd_ssh.returncode
 
 def add(config, push, pull, ssh, port, name, path):
     """
@@ -151,7 +87,7 @@ def add(config, push, pull, ssh, port, name, path):
         new_slave['host'] = host
         new_slave['port'] = str(port)
     elif type == 'local':
-        uuid, relpath = uuid_relpath_pair_from_path(path)
+        uuid, relpath = su.uuid_relpath_pair_from_path(path)
         new_slave['uuid'] = uuid
         new_slave['relpath'] = relpath
 
@@ -219,13 +155,13 @@ def sync_path_local(slave):
     """
     Returns the local filesystem path to the slave.
     """
-    mnt_point = mountpoint_from_uuid(slave['uuid'])
+    mnt_point = su.mountpoint_from_uuid(slave['uuid'])
     if not mnt_point:
         print(slave['name'] + " is not mounted, skipping.")
         return
 
     print(slave['name'] + " found at " + mnt_point)
-    return path_from_uuid_relpath(slave['uuid'], slave['relpath'])
+    return su.path_from_uuid_relpath(slave['uuid'], slave['relpath'])
 
 def sync_path_ssh(slave):
     """
---
+++ jackup/sysutils.py
@@ -0,0 +1,65 @@
+import os
+import sys
+import subprocess
+
+def mountpoint_from_uuid(uuid):
+    """
+    Returns the path where a device with UUID is mounted.
+    If the device is not mounted, then return the empty string.
+    """
+    cmd_findmnt = subprocess.run(["findmnt", "-rn", "-S", "UUID=" + uuid, "-o", "TARGET"], stdout=subprocess.PIPE)
+    return str(cmd_findmnt.stdout, 'utf-8', 'ignore').strip()
+
+# uuids = ["28B88482B884506C", "027E2FC17E2FAC7B"]
+# for uuid in uuids:
+#     mnt_point = mountpoint_from_uuid(uuid)
+#     if mnt_point:
+#         print(uuid + " is mounted at " + mnt_point)
+#     else:
+#         print(uuid + " is not mounted")
+
+
+
+
+def uuid_relpath_pair_from_path(path):
+    """
+    Generates a (UUID, relative path) pair from a path on the file system.
+    This is done by figuring out which device the path belongs to, and then
+    finding its mountpoint, the relative path is then calculated based on where
+    the device is mounted.
+    """
+    cmd_df = subprocess.run(['df', '--output=source', path], stdout=subprocess.PIPE)
+    device = str(cmd_df.stdout, 'utf-8', 'ignore')[10:].strip()
+
+    cmd_lsblk = subprocess.run(['lsblk', '-f', device, '-oUUID'], stdout=subprocess.PIPE)
+    uuid = str(cmd_lsblk.stdout, 'utf-8', 'ignore')[4:].strip()
+
+    cmd_mountpoint = subprocess.run(['lsblk', '-f', device, '-oMOUNTPOINT'], stdout=subprocess.PIPE)
+    mountpoint = str(cmd_mountpoint.stdout, 'utf-8', 'ignore')[10:].strip()
+
+    uuid_relative_path = path
+    if path.startswith(mountpoint):
+        uuid_relative_path = path[len(mountpoint):].strip('/')
+
+    return (uuid, uuid_relative_path)
+
+# paths = ["/mnt/extern/images", "/home/jens/test"]
+# for p in paths:
+#     print(uuid_relpath_pair_from_path(p))
+
+
+
+
+def path_from_uuid_relpath(uuid, relpath):
+    """
+    Reifies a filesystem path from a (UUID, relative path) pair.
+    This is done by finding the mountpoint of the device belonging to the UUID, then
+    glueing the relative path onto mountpoint.
+    """
+    mnt_point = mountpoint_from_uuid(uuid)
+    return os.path.join(mnt_point, relpath)
+
+# uuid_paths = [["027E2FC17E2FAC7B", "images"],
+#               ["d047f2a2-6a9b-4f9c-9c3b-bd3c418babe9", "home/jens/vault"]]
+# for uu,pp in uuid_paths:
+#     print(uu + " + " + pp + " = " + path_from_uuid_relpath(uu,pp))
