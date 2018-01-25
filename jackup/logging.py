4725e1a37e11797127adbf72d608410d77e64734
Jens <jensecj@gmail.com>
2018-01-25 16:40:41 +0100

    started work on better logging

--- jackup/logging.py
+++ jackup/logging.py
@@ -10,14 +10,33 @@ class TC:
     YELLOW = CSI + '33m'
     BLUE = CSI + '34m'
 
+class VERBOSITY:
+    ERROR = 0
+    QUIET = 1
+    WARNIING = 2
+    DEFAULT = 3
+    VERBOSE = 4
+    DEBUG = 5
+
+# given a verbosity level, print all messages at, or below that level
+def log(string, verbosity):
+    if verbosity == VERBOSITY.ERROR:
+        print(string, file=sys.stderr)
+        return
+
+    print(string, file=sys.stdout)
+
 def success(string):
-    print(TC.BOLD + TC.GREEN + string + TC.RESET)
+    log(GREEN(string), VERBOSITY.DEFAULT)
 
 def error(string):
-    print(TC.BOLD + TC.RED + string + TC.RESET, file=sys.stderr)
+    log(RED(string), VERBOSITY.ERROR)
 
 def warning(string):
-    print(TC.BOLD + TC.YELLOW + string + TC.RESET)
+    log(YELLOW(string), VERBOSITY.WARNING)
+
+def debug(string):
+    log(BLUE(string), VERBOSITY.DEBUG)
 
 def RED(string):
     return TC.BOLD + TC.RED + string + TC.RESET;
@@ -27,6 +46,9 @@ def YELLOW(string):
 
 def GREEN(string):
     return TC.BOLD + TC.GREEN + string + TC.RESET;
+
+def BLUE(string):
+    return TC.BOLD + TC.BLUE + string + TC.RESET;
 
 def BOLD(string):
     return TC.BOLD + string + TC.RESET;
