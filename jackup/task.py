ece1980a57f51e24bb9fb97b9447a85f402242d6
Jens Christian Jensen <jensecj@gmail.com>
2018-11-14 21:32:15 +0100

    added to/from json serialization to task.py

--- jackup/task.py
+++ jackup/task.py
@@ -1,8 +1,19 @@
 from dataclasses import dataclass
 
+import json
+
 @dataclass
 class Task:
     name: str
     source: str
     destination: str
     order: int
+
+def toJSON(task: Task):
+    return { 'name': task.name,
+             'source': task.source,
+             'destination': task.destination,
+             'order': str(task.order) }
+
+def fromJSON(json) -> Task:
+    return Task(json['name'], json['source'], json['destination'], int(json['order']))
