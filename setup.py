295cf5ad920699bad6f6b22f0f41c69091defe46
Jens Christian Jensen <jensecj@posteo.net>
2020-01-25 13:59:42 +0100

    updated setup.py, load version from __version__.py, use python 3.8

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
-import sys
-from setuptools import setup
+#!/usr/bin/env python
 
-version = sys.version_info[:2]
-if version < (3, 7):
-    print('jackup requires Python version 3.7 or later' + ' ({}.{} detected).'.format(*version))
+import os
+import sys
+from setuptools import setup, find_packages
+
+
+py_version = sys.version_info[:2]
+if py_version < (3, 8):
+    print(f"jackup requires Python 3.8+ ({py_version} detected).")
     sys.exit(-1)
 
-setup(name='jackup',
-      version='0.6.1',
-      description='Simple synchronization',
-      url='http://github.com/jensecj/jackup',
-      author='Jens Christian Jensen',
-      author_email='jensecj@gmail.com',
-      packages=['jackup'],
-      entry_points = {
-          'console_scripts': ['jackup = jackup.cli:main'],
-      },
-      zip_safe=False)
+
+version = open("__version__.py", "r").read().strip()
+setup(
+    name="jackup",
+    version=version,
+    description="Simple synchronization",
+    url="http://github.com/jensecj/jackup",
+    author="Jens Christian Jensen",
+    author_email="jensecj@gmail.com",
+    packages=find_packages(),
+    entry_points={"console_scripts": ["jackup = jackup.cli:main"],},
+    zip_safe=False,
+)
