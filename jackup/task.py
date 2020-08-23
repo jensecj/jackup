984e486c7fb5adeba135ad976410713b764f54f0
Jens C. Jensen <jensecj@posteo.net>
2020-08-23 13:55:54 +0200

    don't manually serialize Tasks

--- jackup/task.py
+++ jackup/task.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 
-import json
 
 @dataclass(frozen=True)
 class Task:
@@ -8,14 +7,3 @@ class Task:
     source: str
     destination: str
     order: int
-
-def toJSON(task: Task):
-    """Serialize a Task to JSON."""
-    return { 'name': task.name,
-             'source': task.source,
-             'destination': task.destination,
-             'order': task.order }
-
-def fromJSON(json) -> Task:
-    """Serialize a Task from JSON."""
-    return Task(json['name'], json['source'], json['destination'], int(json['order']))
